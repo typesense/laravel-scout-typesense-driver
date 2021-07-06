84398c552c008c74a888ba67229319ce98d7f61d
hi019 <65871571+hi019@users.noreply.github.com>
2021-07-06 17:59:08 -0400

    - Add the required params to the `new Client` call in `register` - Put the register method before boot - Reduce code duplication (2 new Client -> 1 new Client)

--- src/TypesenseServiceProvider.php
+++ src/TypesenseServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Typesense\LaravelTypesense;
 
+use Illuminate\Contracts\Foundation\Application;
 use Typesense\Client;
 use Laravel\Scout\EngineManager;
 use Illuminate\Support\ServiceProvider;
@@ -17,38 +18,46 @@ use Typesense\LaravelTypesense\Engines\TypesenseEngine;
  */
 class TypesenseServiceProvider extends ServiceProvider
 {
-
-    public function boot(): void
-    {
-        $this->app[EngineManager::class]->extend('typesense', static function ($app) {
-            $client = new Client([
-              'master_node'        => config('scout.typesense.master_node', []),
-              'read_replica_nodes' => !config('scout.typesense.enabled_read_replica', false) ? [] : config('scout.typesense.read_replicas', []),
-              'timeout_seconds'    => config('scout.typesense.timeout', 2.0),
-            ]);
-            return new TypesenseEngine(new Typesense($client));
-        });
-
-        Builder::macro('count', function () {
-            return $this->engine()
-                        ->getTotalCount($this->engine()
-                                             ->search($this));
-        });
-    }
-
     public function register(): void
     {
-        $this->app->singleton(Typesense::class, static function () {
-            $client = new Client([
-              'master_node'        => config('scout.typesense.master_node', []),
-              'read_replica_nodes' => !config('scout.typesense.enabled_read_replica', false) ? [] : config('scout.typesense.read_replicas', []),
-              'timeout_seconds'    => config('scout.typesense.timeout', 2.0),
-            ]);
+        $this->app->singleton(
+            Client::class,
+            function () {
+                return new Client(
+                    [
+                        'api_key'                       => config('scout.typesense.api_key', ''),
+                        'nodes'                         => config('scout.typesense.nodes', []),
+                        'nearest_node'                  => config('scout.typesense.nearest_node', []),
+                        'connection_timeout_seconds'    => config('scout.typesense.connection_timeout_seconds', 2.0),
+                        'healthcheck_interval_seconds'  => config('scout.typesense.healthcheck_interval_seconds', 60),
+                        'num_retries'                   => config('scout.typesense.num_retries', 3),
+                        'retry_interval_seconds'        => config('scout.typesense.retry_interval_seconds', 1.0),
+                    ]
+                );
+            }
+        );
 
-            return new Typesense($client);
-        });
+        $this->app->singleton(
+            Typesense::class,
+            function (Client $client) {
+                return new Typesense($client);
+            }
+        );
 
         $this->app->alias(Typesense::class, 'typesense');
     }
 
+    public function boot(): void
+    {
+        $this->app[EngineManager::class]->extend('typesense', static function (Application $app) {
+            return new TypesenseEngine(new Typesense($app->make(Client::class)));
+        });
+
+        // TODO test this manually to make sure it still works
+        Builder::macro('count', function () {
+            return $this->engine()
+                ->getTotalCount($this->engine()
+                    ->search($this));
+        });
+    }
 }
