bb763b634e217761a1f740943404dc6aa6f14d9a
Boris Kremenovic <bkr@goodwings.com>
2023-04-28 14:17:29 +0200

    Align SoftDelete logic with Model::preventAccessingMissingAttributes();

--- src/Engines/TypesenseEngine.php
+++ src/Engines/TypesenseEngine.php
@@ -168,7 +168,7 @@ class TypesenseEngine extends Engine
             $models->each->pushSoftDeleteMetadata();
         }
 
-        if (is_null($models->first()?->deleted_at)) {
+        if (!$this->usesSoftDelete($models->first()) || is_null($models->first()?->deleted_at)) {
             $this->typesense->importDocuments($collection, $models->map(fn($m) => $m->toSearchableArray())
                 ->toArray());
         }
